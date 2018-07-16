3cc030936e2fd04c9f1b7e7480cdac2b1ad073d8
akakumar03 <41191570+akakumar03@users.noreply.github.com>
2018-07-16 21:23:40 +0100

    Moving Object/Image
    A Moving Image which can be controlled using the arrow keys in ASP.NET using C#

---
+++ MovingObject/FormView.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MovingObject
+{
+    partial class FormView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimerMoving = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TimerMoving
+            // 
+            this.TimerMoving.Enabled = true;
+            this.TimerMoving.Tick += new System.EventHandler(this.TimerMoving_Tick);
+            // 
+            // FormView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 385);
+            this.Name = "FormView";
+            this.Text = "Moving Object";
+            this.Load += new System.EventHandler(this.FormView_Load);
+            this.RightToLeftChanged += new System.EventHandler(this.FormView_RightToLeftChanged);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormView_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormView_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimerMoving;
+    }
+}
+
---
+++ MovingObject/FormView.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingObject
+{
+    public partial class FormView : Form
+    {
+        enum Position //enumeration contains its own values and cannot inherit or cannot pass inheritance.
+        {
+
+            Left, Right, Up, Down
+        }
+
+        private int x;
+        private int y;
+        private Position ObjPosition;
+       
+        public FormView()
+        {
+            InitializeComponent();
+            x = 50; //Assigning value to the variable
+            y = 50;
+            ObjPosition = Position.Down; //This by default makes the thing to go down
+        }
+
+        private void FormView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormView_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.FillRectangle(Brushes.Black, x, y, 30,30); //Create a rectangle with black
+            e.Graphics.DrawImage(new Bitmap("12048957873_ab44dde9af.jpg"),x,y,40,40); //Attached a imgae 
+
+        }
+
+        private void TimerMoving_Tick(object sender, EventArgs e) //The timer function makes the box to move from one pace to the other
+        {
+            if (ObjPosition == Position.Right)
+            {
+                x += 10;
+
+            }
+            else if (ObjPosition == Position.Left)
+            {
+                x -= 10;
+            }
+            else if (ObjPosition == Position.Up)
+            {
+                y -= 10;
+            }
+            else if (ObjPosition == Position.Down)
+            {
+                y += 10;
+            }
+            Invalidate(); //This is the reason why the object is moving from left to the right.
+        }
+
+        private void FormView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                ObjPosition = Position.Left;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                ObjPosition = Position.Right;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+
+                ObjPosition = Position.Up;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                ObjPosition = Position.Down;
+            }
+           
+           
+        }
+
+        private void FormView_RightToLeftChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
